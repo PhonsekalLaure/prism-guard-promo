8ef1c54212c2f9e0c66106e006bfb15747f231eb
arixien <richi.elleanng@gmail.com>
2026-04-21 22:06:32 +0800

    feat: implement base styling and layout structure for booking and join pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from '@layouts/MainLayout';
 import Home from '@pages/Home';
-import Services from '@pages/Services';
-import OurClients from '@pages/OurClients';
+//import Services from '@pages/Services';
+//import OurClients from '@pages/OurClients';
+import JoinTheForce from '@pages/JoinTheForce';
 
 export default function App() {
   return (
@@ -10,8 +11,9 @@ export default function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route index element={<Home />} />
-          <Route path="services" element={<Services />} />
-          <Route path="clients" element={<OurClients />} />
+          {/* <Route path="services" element={<Services />} /> */}
+          {/* <Route path="clients" element={<OurClients />} /> */}
+          <Route path="join" element={<JoinTheForce />} />
         </Route>
       </Routes>
     </BrowserRouter>
